0126a03af3ccc51d05b278feadb5bcc531ce2aa0
swamlyoma <stephano.wamlyoma@chakray.com>
2025-10-09 15:40:28 +0300

    updated hardcode version and port

---
+++ camel-microservices-a/src/main/java/com/in28minitus/microservices/camel_microservices_a/CamelMicroservicesAApplication.java
@@ -0,0 +1,13 @@
+package com.in28minitus.microservices.camel_microservices_a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelMicroservicesAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelMicroservicesAApplication.class, args);
+	}
+
+}
---
+++ camel-microservices-a/src/test/java/com/in28minitus/microservices/camel_microservices_a/CamelMicroservicesAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minitus.microservices.camel_microservices_a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelMicroservicesAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
