8b14259fc9c1ce11a458f22b5a7625f984893285
swamlyoma <stephano.wamlyoma@chakray.com>
2025-10-13 21:17:55 +0300

    added message sender

---
+++ camel-microservices-a/src/main/java/com/in28minitus/microservices/camel_microservices_a/routes/ActiveMqSenderRouter.java
@@ -0,0 +1,19 @@
+package com.in28minitus.microservices.camel_microservices_a.routes;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ActiveMqSenderRouter extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+    //timer
+    from("timer:active-mq-timer?period=10000")
+    .transform().constant("my message for active Mq")
+    .log("${body}")
+    .to("activemq:my-activemq-queue");
+    //que
+    }
+    
+}
