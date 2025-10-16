1be04ec483e607433eb247225331a38233fb2679
swamlyoma <stephano.wamlyoma@chakray.com>
2025-10-16 16:51:14 +0300

    added serialzer class

--- camel-microservices-b/src/main/java/com/in28minitus/microservices/camel_microservices_b/routes/ActiveMqReceiverRouter.java
+++ camel-microservices-b/src/main/java/com/in28minitus/microservices/camel_microservices_b/routes/ActiveMqReceiverRouter.java
@@ -1,15 +1,47 @@
 package com.in28minitus.microservices.camel_microservices_b.routes;
 
+import java.math.BigDecimal;
+
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ActiveMqReceiverRouter extends RouteBuilder {
 
+@Autowired    
+private MyCurrencyExchangeProcessor myCurrencyExchangeProcessor;
+
     @Override
     public void configure() throws Exception {
         from("activemq:my-activemq-queue")
+        .unmarshal().json(JsonLibrary.Jackson, CurrencyExchange.class)
+        .bean(myCurrencyExchangeProcessor)
         .to("log:received-message-from-active-mq");
     }
-    
+
+//     {
+//     "id":1000,
+//     "from":"USD",
+//     "to":"INR",
+//     "conversionMultiple":70
+// }
+
+
+}
+@Component
+class MyCurrencyExchangeProcessor{
+        Logger logger = LoggerFactory.getLogger(MyCurrencyExchangeProcessor.class);
+
+    public void processMessage(CurrencyExchange currencyExchange){
+
+        
+
+        logger.info("Do some proceesing with {}",currencyExchange.getConversionMultiple());
+
+        return;
+    }
 }
